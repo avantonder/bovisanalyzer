8f69a8a18c4686e8a14e607952591c71da57ab93
avantonder <ajv37@cam.ac.uk>
2022-11-14 15:47:24 +0000

    rewrite python scripts

--- bin/kraken_parser.py
+++ bin/kraken_parser.py
@@ -1,58 +1,87 @@
 #!/usr/bin/env python
 
-import os, argparse, sys, subprocess, glob
+import sys
+import glob
+import os
+import argparse
 import pandas as pd
 import numpy as np
 
-kraken_report_files = sorted(glob.glob('*.kraken2.report.txt'))
+def parser_args(args=None):
+    """ 
+    Function for input arguments for kraken_parser.py
+    """
+    Description = 'Collect Kraken 2 and Bracken outputs and create a summary table'
+    Epilog = """Example usage: python kraken_parser.py """
+    parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
+    parser.add_argument("-of", "--output_file", type=str, default="species_composition.tsv", help="Species composition file (default: 'species_composition.tsv').")
+    return parser.parse_args(args)
 
-species_abundance_files = sorted(glob.glob('*.tsv'))
+def make_dir(path):
+    """ 
+    Function for making a directory from a provided path
+    """
+    if not len(path) == 0:
+        try:
+            os.makedirs(path)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
 
-kraken_report_df = [pd.read_csv(f, sep='\t', header=None) for f in kraken_report_files]
+def files_to_dataframe(kraken_files, bracken_files):
+	""" 
+    Function for making a dataframe from lists of Kraken 2 and Bracken report files
+    """
+	kraken_report_list = [pd.read_csv(f, sep='\t', header=None) for f in kraken_files]
+	kraken_report_list = [i.head(1).iloc[:,[5,1]] for i in kraken_report_list]
+	species_abundance_list = [pd.read_csv(f, sep='\t') for f in bracken_files]
+	species_abundance_names = [i.replace('_S.tsv', '') for i in bracken_files]
+	
+	for a,b in zip(kraken_report_list, species_abundance_names):
+		a.rename(columns = {5: 'name', 1: b}, inplace=True)
 
-kraken_report_df_filtered = [i.head(1).iloc[:,[5,1]] for i in kraken_report_df]
+	for a,b in zip(species_abundance_list, species_abundance_names):
+		a.rename(columns = {'new_est_reads': b, 'fraction_total_reads': b + '_frac'}, inplace=True)
+	
+	species_abundance_list = [i.iloc[:,[0,5]] for i in species_abundance_list]
+	species_abundance_joined = pd.concat([i.set_index('name') for i in species_abundance_list], axis=1).reset_index()
+	species_abundance_joined.rename(columns = {'index': 'name'}, inplace=True)
+	kraken_report_joined = pd.concat([i.set_index('name') for i in kraken_report_list], axis=1).reset_index()
+	species_abundance_unclassified = species_abundance_joined.append(kraken_report_joined, ignore_index=True)
+	species_abundance_unclassified.fillna(0, inplace=True)
 
-species_abundance_df = [pd.read_csv(f, sep='\t') for f in species_abundance_files]
+	for column in species_abundance_unclassified.columns[1:]:
+		species_abundance_unclassified[column + '_freq'] = species_abundance_unclassified[column] / species_abundance_unclassified[column].sum() * 100
 
-species_abundance_names = [i.replace('_S.tsv', '') for i in species_abundance_files]
+	species_abundance_unclassified_filtered = pd.concat([species_abundance_unclassified['name'], species_abundance_unclassified.filter(like='_freq')], axis=1)
+	species_abundance_unclassified_filtered['Max_value'] = species_abundance_unclassified_filtered.filter(like='_freq').max(axis=1)
+	species_abundance_unclassified_filtered = species_abundance_unclassified_filtered[(species_abundance_unclassified_filtered['Max_value'] >= 5)]
+	total = 100 - species_abundance_unclassified_filtered.filter(like='_freq').apply(np.sum)
+	total['name'] = 'other'
+	final_abundance = species_abundance_unclassified_filtered.append(pd.DataFrame(total.values, index=total.keys()).T, ignore_index=True)
+	final_abundance_sorted = pd.concat([final_abundance['name'], final_abundance.filter(like = '_freq')], axis = 1)
+	final_abundance_sorted.columns = final_abundance_sorted.columns.str.replace('_freq', '')
 
-for a,b in zip(kraken_report_df_filtered, species_abundance_names):
-	a.rename(columns = {5: 'name', 1: b}, inplace=True)
+	return final_abundance_sorted
 
-for a,b in zip(species_abundance_df, species_abundance_names):
-	a.rename(columns = {'new_est_reads': b, 'fraction_total_reads': b + '_frac'}, inplace=True)
+def main(args=None):
+    args = parser_args(args)
 
-species_abundance_df_filtered = [i.iloc[:,[0,5]] for i in species_abundance_df]
+    ## Create output directory if it doesn't exist
+    out_dir = os.path.dirname(args.output_file)
+    make_dir(out_dir)
 
-species_abundance_joined = pd.concat([i.set_index('name') for i in species_abundance_df_filtered], axis=1).reset_index()
+    ## Create list of Kraken 2 report files
+	kraken_report_files = sorted(glob.glob('*.kraken2.report.txt'))
 
-species_abundance_joined.rename(columns = {'index': 'name'}, inplace=True)
+    ## Create list of Bracken species abundance files
+	species_abundance_files = sorted(glob.glob('*.tsv'))
+	
+	## Create combined Kraken 2 and Bracken dataframe
+    combined_df = files_to_dataframe(kraken_report_files,species_abundance_files)
 
-kraken_report_joined = pd.concat([i.set_index('name') for i in kraken_report_df_filtered], axis=1).reset_index()
+    ## Write output file
+    combined_df.T.to_csv(args.output_file, sep = '\t', header = False)
 
-species_abundance_unclassified = species_abundance_joined.append(kraken_report_joined, ignore_index=True)
-
-species_abundance_unclassified.fillna(0, inplace=True)
-
-for column in species_abundance_unclassified.columns[1:]:
-	species_abundance_unclassified[column + '_freq'] = species_abundance_unclassified[column] / species_abundance_unclassified[column].sum() * 100
-
-species_abundance_unclassified_filtered = pd.concat([species_abundance_unclassified['name'], species_abundance_unclassified.filter(like='_freq')], axis=1)
-
-species_abundance_unclassified_filtered['Max_value'] = species_abundance_unclassified_filtered.filter(like='_freq').max(axis=1)
-
-species_abundance_unclassified_filtered = species_abundance_unclassified_filtered[(species_abundance_unclassified_filtered['Max_value'] >= 5)]
-
-total = 100 - species_abundance_unclassified_filtered.filter(like='_freq').apply(np.sum)
-
-total['name'] = 'other'
-
-final_abundance = species_abundance_unclassified_filtered.append(pd.DataFrame(total.values, index=total.keys()).T, ignore_index=True)
-
-final_abundance_sorted = pd.concat([final_abundance['name'], final_abundance.filter(like = '_freq')], axis = 1)
-
-final_abundance_sorted.columns = final_abundance_sorted.columns.str.replace('_freq', '')
-
-final_abundance_tsv_name = 'species_composition.tsv'
-
-final_abundance_sorted.T.to_csv(final_abundance_tsv_name, sep = '\t', header = False)
+if __name__ == '__main__':
+    sys.exit(main())
--- bin/spoligo_parser.py
+++ bin/spoligo_parser.py
@@ -1,61 +1,92 @@
 #!/usr/bin/env python3
 
-import os, argparse, sys, subprocess, glob
+import os 
+import argparse 
+import sys 
+import glob
 import pandas as pd
-import numpy as np
 
-# Read in spoligotype database file
+def parser_args(args=None):
+    """ 
+    Function for input arguments for spoligo_parser.py
+    """
+    Description = 'Collect SpoTyping outputs, match against spoligotype database and create a summary table'
+    Epilog = """Example usage: python spoligo_parser.py """
+    parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
+    parser.add_argument("-sb", "--spol_db"    , type=str, default="spoligotype_db.tsv"     , help="Spoligotype database file (default: 'spoligotype_db.tsv').")
+    parser.add_argument("-of", "--output_file", type=str, default="spoligotype_summary.tsv", help="Spoligotype summary file (default: 'spoligotype_summary.tsv').")
+    return parser.parse_args(args)
 
-spoligo_db = "spoligotype_db.tsv"
+def make_dir(path):
+    """ 
+    Function for making a directory from a provided path
+    """
+    if not len(path) == 0:
+        try:
+            os.makedirs(path)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
 
-spoligo_db_read = pd.read_csv(spoligo_db, sep=' ', header=None)
+def spol_db_parse(spol_db):
+    """ 
+    Function for reading and formatting spoligotype database
+    """
+    spoligo_db_read = pd.read_csv(spol_db, sep=' ', header=None)
+    spoligo_db_df = spoligo_db_read.iloc[:, [2,1]]
+    spoligo_db_df.columns = ['binary', 'SB number']
 
-spoligo_db_df = spoligo_db_read.iloc[:, [2,1]]
+    return spoligo_db_df
 
-spoligo_db_df.columns = ['binary', 'SB number']
+def spol_to_dataframe(file_list):
+    """ 
+    Function to take list of SpoTyping files and create a summary table
+    """
+    spoligo_names = [i.replace('.txt', '') for i in file_list]
+    spoligo_names_df = pd.DataFrame(spoligo_names)
+    spoligo_names_df.columns = ['sample']
+    spoligo_files_read = [open(file).read() for file in file_list]
+    spoligo_files_read_df = pd.DataFrame(spoligo_files_read)
+    spoligo_files_read_df = spoligo_files_read_df[0].str.split('\t', expand=True)
+    spoligo_files_read_df = spoligo_files_read_df.replace('\n','', regex=True)
+    spoligo_files_read_df.columns = ['files', 'binary', 'octal']
+    spoligo_merged_df = spoligo_names_df.join(spoligo_files_read_df)
+    cols = ['sample', 'binary', 'octal']
+    spoligo_ordered_df = spoligo_merged_df[cols]
 
-# Read in spotyping output files
+    return spoligo_ordered_df
 
-spoligo_files = sorted(glob.glob('*.txt'))
+def main(args=None):
+    args = parser_args(args)
 
-spoligo_names = [i.replace('.txt', '') for i in spoligo_files]
+    ## Create output directory if it doesn't exist
+    out_dir = os.path.dirname(args.output_file)
+    make_dir(out_dir)
 
-spoligo_names_df = pd.DataFrame(spoligo_names)
+    ## Create list of SpoTyping output files
+    spoligo_files = sorted(glob.glob('*.txt'))
 
-spoligo_names_df.columns = ['sample']
+    ## Create dataframe
+    spoligo_df = spol_to_dataframe(spoligo_files)
 
-spoligo_files_read = [open(file).read() for file in spoligo_files]
+    ## Parse and format spoligotype database
+    spoligo_db = spol_db_parse(args.spol_db)
+    
+    ## Merge spotyping results and database
+    spoligo_lookup_df = pd.merge(
+        left=spoligo_df,
+        right=spoligo_db,
+        left_on='binary',
+        right_on='binary',
+        how='left'
+    ).fillna('Not found')
 
-spoligo_files_read_df = pd.DataFrame(spoligo_files_read)
+    ## Final column order
+    cols = ['sample', 'SB number', 'binary', 'octal']
+    spoligo_ordered_df = spoligo_lookup_df[cols]
 
-spoligo_files_read_df = spoligo_files_read_df[0].str.split('\t', expand=True)
+    ## Write output file
+    spoligo_ordered_df.to_csv(args.output_file, sep = '\t', header = True, index = False)
 
-spoligo_files_read_df = spoligo_files_read_df.replace('\n','', regex=True)
-
-spoligo_files_read_df.columns = ['files', 'binary', 'octal']
-
-spoligo_merged_df = spoligo_names_df.join(spoligo_files_read_df)
-
-cols = ['sample', 'binary', 'octal']
-
-spoligo_ordered_df = spoligo_merged_df[cols]
-
-# Merge spotyping results and database
-
-spoligo_lookup_df = pd.merge(
-    left=spoligo_ordered_df,
-    right=spoligo_db_df,
-    left_on='binary',
-    right_on='binary',
-    how='left'
-).fillna('Not found')
-
-cols = ['sample', 'SB number', 'binary', 'octal']
-
-spoligo_ordered_df = spoligo_lookup_df[cols]
-
-# Save to tsv file
-
-spoligo_tsv_name = 'spoligotype_summary.tsv'
-
-spoligo_ordered_df.to_csv(spoligo_tsv_name, sep = '\t', header = True, index = False)
+if __name__ == '__main__':
+    sys.exit(main())
